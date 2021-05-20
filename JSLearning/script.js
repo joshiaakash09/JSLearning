2b1d73a3c73bc0af8245e5b049572daa35454b32
pheonix <aakash_joshi@outlook.com>
2021-05-20 09:06:32 -0500

    adding new file

---
+++ JSLearning/script.js
@@ -0,0 +1,20 @@
+const backpack = {
+    name: "Everyday Backpack",
+    color: "grey",
+    poacketNum: 15,
+    strapLength: {
+        left: 26,
+        right: 26,
+    },
+    lidopen: false,
+    togglelid: function(lidStatus) {
+        this.lidopen = lidStatus;
+    },
+    newSrapLength: function (lenghtLeft, lenghtRight) {
+        this.strapLength.left = lenghtLeft;
+        this.strapLength.right = lenghtRight;
+    },
+};
+
+console.log("The backpack object:", backpack);
+console.log("The poacketNum value:", backpack.poacketNum)
